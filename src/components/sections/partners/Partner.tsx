0130c1108eb67c2955a0c2a641e69aa0bb7a3277
Catherine Vuthi <vuthi.esther@gmail.com>
2026-04-21 21:22:59 +0300

    Update partner component

--- src/components/sections/partners/Partner.tsx
+++ src/components/sections/partners/Partner.tsx
@@ -1,26 +1,20 @@
 'use client';
-
-import Image from 'next/image';
+	
 import Container from '@/components/ui/Container';
-import { logos } from './partners.data';
+import { partnersData } from './partners.data';
 
 export function Partner() {
 	return (
 		<section className="py-16">
 			<Container>
-				<div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6">
-					{logos.map((logo) => (
+				<div className="flex flex-wrap items-center justify-center gap-x-28 gap-y-6">
+					{partnersData.map((partner) => (
 						<div
-							key={logo.name}
-							className="flex items-center justify-center"
+							key={partner.name}
+							className="flex items-end justify-center gap-1"
 						>
-							<Image
-								src={logo.src}
-								alt={logo.name}
-								width={250}
-								height={60}
-								className="object-contain rounded-lg"
-							/>
+							<h3 className="text-primary">{partner.amount}</h3>
+							<p className="text-sm">{partner.name}</p>
 						</div>
 					))}
 				</div>
