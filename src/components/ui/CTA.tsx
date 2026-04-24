7a9369500423d072db4e8d542aa49364e1c7a240
Catherine Vuthi <vuthi.esther@gmail.com>
2026-04-24 21:49:29 +0300

    Change CTA styles

--- src/components/ui/CTA.tsx
+++ src/components/ui/CTA.tsx
@@ -1,19 +1,23 @@
 'use client';
 
-import React from 'react';
+import Button from './Button';
 
 const CTA = () => {
 	return (
 		<section className="py-16">
-			<div className="bg-primary text-primary-foreground rounded-2xl p-10 text-center space-y-6">
+			<div className="flex flex-col justify-center items-center bg-primary/80 rounded-2xl p-10 text-center space-y-6">
 				<h2>Launch your online presence today</h2>
 				<p className="max-w-2xl mx-auto text-primary-foreground">
 					Get your website live in just one day. No complexity, no delays - just
 					a fast, clean launch.
 				</p>
-				<button className="px-6 py-3 bg-white text-primary rounded-lg font-medium hover:opacity-90 cursor-pointer transition">
-					Get Started Now
-				</button>
+				<Button
+					variant="primary"
+					size="lg"
+					className="w-1/4"
+				>
+					Get Started
+				</Button>
 			</div>
 		</section>
 	);
