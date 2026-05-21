f5ee6166cd6ddaa6c033c85fad2b3cc4e71fd9b6
Catherine Vuthi <vuthi.esther@gmail.com>
2026-05-21 20:42:32 +0300

    Change CTA copy

--- src/components/ui/CTA.tsx
+++ src/components/ui/CTA.tsx
@@ -1,23 +1,33 @@
 'use client';
 
+import { FaApple, FaGooglePlay } from 'react-icons/fa';
 import Button from './Button';
 
 const CTA = () => {
 	return (
 		<section className="py-16">
-			<div className="flex flex-col justify-center items-center bg-primary/80 rounded-2xl p-10 text-center space-y-6">
-				<h2>Launch your online presence today</h2>
-				<p className="max-w-2xl mx-auto text-primary-foreground">
-					Get your website live in just one day. No complexity, no delays - just
-					a fast, clean launch.
-				</p>
-				<Button
-					variant="primary"
-					size="lg"
-					className="w-1/4"
-				>
-					Get Started
-				</Button>
+			<div className="flex flex-col justify-center items-center bg-primary/40 rounded-2xl p-16 text-center space-y-6">
+				<h2>Start your Bible study journey today</h2>
+				<div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start pt-4">
+					<Button
+						size="lg"
+						icon={<FaApple />}
+						onClick={() =>
+							(window.location.href = 'https://apps.apple.com/us/iphone/apps')
+						}
+					>
+						Download on iOS
+					</Button>
+					<Button
+						size="lg"
+						icon={<FaGooglePlay />}
+						onClick={() =>
+							(window.location.href = 'https://play.google.com/store/apps/')
+						}
+					>
+						Get it on Android
+					</Button>
+				</div>
 			</div>
 		</section>
 	);
