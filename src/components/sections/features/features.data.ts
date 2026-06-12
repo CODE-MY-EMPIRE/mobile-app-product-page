0b6c02965efebbf35bd14b537e5c7ea4ba65d646
Catherine Vuthi <vuthi.esther@gmail.com>
2026-06-12 10:45:33 +0300

    Update heo and feature data with mobile app images

--- src/components/sections/features/features.data.ts
+++ src/components/sections/features/features.data.ts
@@ -9,18 +9,18 @@ export const featuresSplitData: FeaturesSplitData[] = [
 		title: 'Different Bible Versions',
 		description:
 			'Explore the Bible in a wide range of trusted versions and translations. Easily switch between translations to compare meaning and deepen your understanding of each passage. Read comfortably in your preferred language for a more personal and accessible experience.',
-		image: '/images/dashboard.png',
+		image: '/images/page2.png',
 	},
 	{
 		title: 'Track Your Progress',
 		description:
 			'Stay motivated by tracking your Bible reading journey in real time. Clearly see which books and chapters you’ve completed and what’s left to explore. Visual progress indicators help you build consistency and reach your reading goals.',
-		image: '/images/dashboard-img.png',
+		image: '/images/page3.png',
 	},
 	{
 		title: 'Custom Bible Reading Plan',
 		description:
 			'Create personalized Bible reading plans tailored to your schedule and goals. Choose specific books, themes, or timeframes that fit your spiritual journey. Stay organized and consistent with a plan designed just for you.',
-		image: '/images/dashboard-img.png',
+		image: '/images/page4.png',
 	},
 ];
